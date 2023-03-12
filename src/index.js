7d83f1e5349039e4231cfe676d7a1c57bdf30662
Brian Thornton <brianthornton@brians-mbp-2.lan>
2023-03-12 11:38:27 -0400

    Added graphql

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import { BrowserRouter } from "react-router-dom";
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { IntlProvider } from "react-intl";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import './index.css';
 import App from './App';
 
 import messages_en from "./translations/en.json";
+
+const client = new ApolloClient({
+  uri: 'graphql',
+  cache: new InMemoryCache(),
+});
 
 const messages = {
   'en': messages_en,
@@ -20,6 +26,8 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<BrowserRouter>
   <IntlProvider locale='en' defaultLocale="en" messages={messages[language]}>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </IntlProvider>
 </BrowserRouter>);
